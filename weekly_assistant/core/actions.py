484ed32e12dc467a3e242ce8dff37a179f783258
felipevzps <felipe.vzps@gmail.com>
2025-04-15 14:12:37 -0300

    add vault path and move last week note to archive

--- weekly_assistant/core/actions.py
+++ weekly_assistant/core/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import os
+import os 
+import shutil
 from core import google_calendar, note_generator, note_parser, organize_weekly_tasks, organize_google_calendar_weekly_tasks
 
 def remove_temporary_files(paths):
@@ -11,14 +12,24 @@ def remove_temporary_files(paths):
         except OSError as e:
             print(f"Erro ao remover {path}: {e}")
 
+def move_note_to_archive(inbox, archive):
+    try:
+        shutil.move(inbox, archive)
+        print(f"Nota movida de {inbox} para {archive}")
+    except Exception as e:
+        print(f"Erro ao mover arquivo: {e}")
+
 def main():
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
     
     calendar_path = os.path.join(BASE_DIR, "../calendar/google_calendar.md")
     token_path = os.path.join(BASE_DIR, "../config/token.json")
     credentials_path = os.path.join(BASE_DIR, "../config/credentials.json")
-    inbox_dir = os.path.join(BASE_DIR, "inbox")
-    weekly_notes_dir = inbox_dir  # TODO: setup obsidian inbox vault
+    #inbox_dir = os.path.join(BASE_DIR, "inbox")                    # just for debugging inside core directory
+    #archive_dir = os.path.join(BASE_DIR, "archive")
+    inbox_dir = "/home/felipevzps/obsidian/workspace/inbox"         # obsidian inbox 
+    archive_dir = "/home/felipevzps/obsidian/workspace/archive"     # obsidian archive
+    weekly_notes_dir = inbox_dir
     pending_tasks_path = os.path.join(BASE_DIR, "../tasks/tarefas_pendentes.md")
     future_tasks_path = os.path.join(BASE_DIR, "../tasks/tarefas_futuras.md")
     
@@ -26,7 +37,7 @@ def main():
     google_calendar.main(calendar_path, token_path, credentials_path)
     
     print("Executando note_parser.py...")
-    note_parser.main(inbox_dir, pending_tasks_path, future_tasks_path)
+    last_week_path = note_parser.main(inbox_dir, pending_tasks_path, future_tasks_path)
     
     print("Executando note_generator.py...")
     new_note_path = note_generator.main(weekly_notes_dir)
@@ -39,3 +50,6 @@ def main():
     
     print("Removendo arquivos temporários...")
     remove_temporary_files([calendar_path, pending_tasks_path, future_tasks_path])
+
+    print("Movendo nota da semana passada para archive...")
+    move_note_to_archive(last_week_path, archive_dir)
