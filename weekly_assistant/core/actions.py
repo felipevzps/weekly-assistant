f4689eb80c672063a30fbe8f042706a1f4c58e7a
felipevzps <felipe.vzps@gmail.com>
2025-04-16 01:03:21 -0300

    add timestamp

--- weekly_assistant/core/actions.py
+++ weekly_assistant/core/actions.py
@@ -2,22 +2,27 @@
 
 import os 
 import shutil
+from datetime import datetime
 from core import google_calendar, note_generator, note_parser, organize_weekly_tasks, organize_google_calendar_weekly_tasks
 
 def remove_temporary_files(paths):
     for path in paths:
         try:
             os.remove(path)
-            print(f"Arquivo temporário removido: {path}")
+            #print(f"Arquivo temporário removido: {path}")
         except OSError as e:
             print(f"Erro ao remover {path}: {e}")
 
 def move_note_to_archive(inbox, archive):
     try:
         shutil.move(inbox, archive)
-        print(f"Nota movida de {inbox} para {archive}")
+        now = datetime.now()
+        timestamp = now.strftime("%d/%m/%Y - %H:%M:%S")
+        print(f"[{timestamp}] Nota movida de {inbox} para {archive}")
     except Exception as e:
-        print(f"Erro ao mover arquivo: {e}")
+        now = datetime.now()
+        timestamp = now.strftime("%d/%m/%Y - %H:%M:%S")
+        print(f"[{timestamp}] Erro ao mover arquivo: {e}")
 
 def main():
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
@@ -33,23 +38,23 @@ def main():
     pending_tasks_path = os.path.join(BASE_DIR, "../tasks/tarefas_pendentes.md")
     future_tasks_path = os.path.join(BASE_DIR, "../tasks/tarefas_futuras.md")
     
-    print("Executando google_calendar.py...")
+    #print("Executando google_calendar.py...")
     google_calendar.main(calendar_path, token_path, credentials_path)
     
-    print("Executando note_parser.py...")
+    #print("Executando note_parser.py...")
     last_week_path = note_parser.main(inbox_dir, pending_tasks_path, future_tasks_path)
     
-    print("Executando note_generator.py...")
+    #print("Executando note_generator.py...")
     new_note_path = note_generator.main(weekly_notes_dir)
     
-    print("Executando organize_weekly_tasks.py...")
+    #print("Executando organize_weekly_tasks.py...")
     organize_weekly_tasks.main(inbox_dir, pending_tasks_path, future_tasks_path, weekly_note_path=new_note_path)
     
-    print("Executando organize_google_calendar_weekly_tasks.py...")
+    #print("Executando organize_google_calendar_weekly_tasks.py...")
     organize_google_calendar_weekly_tasks.main(inbox_dir, calendar_path, weekly_note_path=new_note_path)
     
-    print("Removendo arquivos temporários...")
+    #print("Removendo arquivos temporários...")
     remove_temporary_files([calendar_path, pending_tasks_path, future_tasks_path])
 
-    print("Movendo nota da semana passada para archive...")
+    #print("Movendo nota da semana passada para archive...")
     move_note_to_archive(last_week_path, archive_dir)
